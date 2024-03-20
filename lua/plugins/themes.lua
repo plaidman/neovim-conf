b09aad115f7f03c95a28946bda66cd4a94fa4e04
Jason Tomsic <jasontomsic@quickenloans.com>
2024-03-19 21:07:55 -0400

    pick an initial theme

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,32 @@
+--[[
+
+https://github.com/aktersnurra/no-clown-fiesta.nvim
+https://github.com/catppuccin/nvim
+https://github.com/cryptomilk/nightcity.nvim
+https://github.com/deparr/tairiki.nvim
+https://github.com/EdenEast/nightfox.nvim
+https://github.com/folke/tokyonight.nvim
+https://codeberg.org/jthvai/lavender.nvim
+https://github.com/marko-cerovac/material.nvim
+https://github.com/mcchrish/zenbones.nvim
+https://github.com/Mofiqul/vscode.nvim
+https://github.com/navarasu/onedark.nvim
+https://github.com/neanias/everforest-nvim
+https://github.com/olimorris/onedarkpro.nvim
+https://github.com/olivercederborg/poimandres.nvim
+https://github.com/projekt0n/github-nvim-theme
+https://github.com/rebelot/kanagawa.nvim
+https://github.com/ribru17/bamboo.nvim
+https://github.com/sainnhe/edge
+https://github.com/sainnhe/sonokai
+https://github.com/shaunsingh/nord.nvim
+
+--]]
+
+return {
+	"sainnhe/sonokai",
+	priority = 1000,
+	config = function()
+		vim.cmd('colorscheme sonokai')
+	end,
+}
