e520661f5fe502db397511285b9c519fbb72ae71
Jason Tomsic <jasontomsic@quickenloans.com>
2024-03-21 07:23:12 -0400

    polished completion config

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -4,9 +4,6 @@ return {
 		build = (not jit.os:find("Windows"))
 			and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
 			or nil,
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-		},
 	},
 	{
 		"hrsh7th/nvim-cmp",
@@ -14,14 +11,28 @@ return {
 			"hrsh7th/cmp-nvim-lsp",
 			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-buffer",
 		},
 		config = function()
 			local cmp = require("cmp")
 
 			cmp.setup({
+				completion = {
+					completeopt = "menu,menuone,noinsert",
+				},
+				-- TODO: add icons to menu
+				-- https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance#how-to-add-visual-studio-code-codicons-to-the-menu
+
+				-- TODO: learn keybinds for filling in snippet placeholders
+				-- https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymap
+
+				-- TODO: read through docs for some good keybind options for buffer and command
+				-- https://github.com/hrsh7th/nvim-cmp/blob/main/doc/cmp.txt
 				snippet = {
 					expand = function(args)
-						-- TODO: you need to have snippets dummy;
 						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
 					end,
 				},
@@ -38,9 +49,42 @@ return {
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
+					{ name = "luasnip" },
+					{ name = "path" },
+					{ name = "nvim_lsp_signature_help" },
 				}, {
 					{ name = "buffer" },
+				}),
+			})
+
+			cmp.setup.cmdline(":", {
+				completion = {
+					autocomplete = false,
+					completeopt = "menu,menuone,noinsert",
+				},
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+			})
+
+			cmp.setup.cmdline("?", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "buffer" },
+				}, {
+					{ name = "cmdline" },
+				}),
+			})
+
+			cmp.setup.cmdline("/", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "buffer" },
+				}, {
+					{ name = "cmdline" },
 				}),
 			})
 		end,
