dbb7dcb9cafe7cd7127847fa6159a400bd2e1ec2
Jason Tomsic <jasontomsic@quickenloans.com>
2024-03-21 17:28:52 -0400

    trying oil.nvim

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -1,31 +1,27 @@
 return {
-	-- TODO: use the mini.files repo directly?
+	-- keybinds for
+	--   enter folder
+	--   back folder
+	--   back previous to CWD
+	--   cd
+	--   hide and show dot files
+	--   preview files
+	--   open explorer in current file's directory
+	--   open explorer in CWD
 
-	-- alternatives:
-	-- https://github.com/nvim-telescope/telescope-file-browser.nvim
-	-- oil.nvim
-	-- neotree
-	-- nvimtree
+	{
+		'stevearc/oil.nvim',
+		opts = {},
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"echasnovski/mini.files",
+		version = false,
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 
-	"echasnovski/mini.files",
-	version = false,
-	config = function()
-		require("mini.files").setup()
-
-		-- TODO: keybinds
-		-- <leader>fe - toggle explorer
-		-- ,cd - change directory
-		-- ,. - show/hide dot files
-		-- <shift-right> - go in (but not open file)
-		-- <shift-left> - go out
-		-- <shift-enter> - open file or change dir
-		-- <enter> - do nothing
-		--
-		-- https://github.com/echasnovski/mini.nvim/blob/main/doc/mini-files.txt
-		-- show/hide dot files
-		-- toggle explorer
-		-- change current working directory
-
-		vim.keymap.set("n", "<leader>fe", MiniFiles.open)
-	end,
+		config = function()
+			require("mini.files").setup()
+			vim.keymap.set("n", "<leader>fe", MiniFiles.open)
+		end,
+	},
 }
