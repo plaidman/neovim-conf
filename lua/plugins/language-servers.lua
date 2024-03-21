003115413e348fe547170a80a99cb381b10b65a5
Jason Tomsic <jasontomsic@quickenloans.com>
2024-03-21 17:19:59 -0400

    fixing vim completions, adding python lsp, fixing 'gd'

--- lua/plugins/language-servers.lua
+++ lua/plugins/language-servers.lua
@@ -1,24 +1,25 @@
 return {
 	{ "williamboman/mason.nvim", build = ":MasonUpdate", opts = {} },
-	{
-		"folke/neodev.nvim",
-		opts = {},
-	},
+
 	{
 		"williamboman/mason-lspconfig.nvim",
 		dependencies = { "williamboman/mason.nvim" },
 	},
+
 	{
 		"neovim/nvim-lspconfig",
+
 		dependencies = {
 			"williamboman/mason-lspconfig.nvim",
 			"folke/neodev.nvim",
 			"hrsh7th/cmp-nvim-lsp",
 		},
+
 		config = function()
-			local servers = { "lua_ls", "tsserver", "gopls", "eslint" }
+			local servers = { "lua_ls", "tsserver", "gopls", "eslint", "pyright" }
 
 			require("mason-lspconfig").setup({ ensure_installed = servers })
+			require("neodev").setup()
 
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
@@ -32,15 +33,17 @@ return {
 			-- hover to show underline
 			-- hover to show definition?
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
+
 	{
 		"nvimtools/none-ls.nvim",
 		dependencies = {
 			"jay-babu/mason-null-ls.nvim",
 		},
+
 		config = function()
 			local null_ls = require("null-ls")
 
@@ -55,11 +58,13 @@ return {
 			vim.keymap.set("n", "<leader>=", vim.lsp.buf.format, {})
 		end,
 	},
+
 	{
 		"jay-babu/mason-null-ls.nvim",
 		dependencies = {
 			"williamboman/mason.nvim",
 		},
+
 		config = function()
 			require("mason-null-ls").setup({
 				automatic_installation = true,
