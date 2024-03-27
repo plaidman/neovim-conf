171d6c92f4a39c26a788583b15549770fa1646a0
Jason Tomsic <jasontomsic@quickenloans.com>
2024-03-27 18:25:26 -0400

    added colorschemes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,32 +1,28 @@
---[[
-
-https://github.com/aktersnurra/no-clown-fiesta.nvim
-https://github.com/catppuccin/nvim
-https://github.com/cryptomilk/nightcity.nvim
-https://github.com/deparr/tairiki.nvim
-https://github.com/EdenEast/nightfox.nvim
-https://github.com/folke/tokyonight.nvim
-https://codeberg.org/jthvai/lavender.nvim
-https://github.com/marko-cerovac/material.nvim
-https://github.com/mcchrish/zenbones.nvim
-https://github.com/Mofiqul/vscode.nvim
-https://github.com/navarasu/onedark.nvim
-https://github.com/neanias/everforest-nvim
-https://github.com/olimorris/onedarkpro.nvim
-https://github.com/olivercederborg/poimandres.nvim
-https://github.com/projekt0n/github-nvim-theme
-https://github.com/rebelot/kanagawa.nvim
-https://github.com/ribru17/bamboo.nvim
-https://github.com/sainnhe/edge
-https://github.com/sainnhe/sonokai
-https://github.com/shaunsingh/nord.nvim
-
---]]
-
 return {
+	"catppuccin/nvim",
+	"cryptomilk/nightcity.nvim",
+	"deparr/tairiki.nvim",
+	"EdenEast/nightfox.nvim",
+	"folke/tokyonight.nvim",
+	"marko-cerovac/material.nvim",
+	"Mofiqul/vscode.nvim",
+	"navarasu/onedark.nvim",
+	"neanias/everforest-nvim",
+	"olimorris/onedarkpro.nvim",
+	"olivercederborg/poimandres.nvim",
+	"projekt0n/github-nvim-theme",
+	"rebelot/kanagawa.nvim",
+	"ribru17/bamboo.nvim",
+	"sainnhe/edge",
 	"sainnhe/sonokai",
-	priority = 1000,
-	config = function()
-		vim.cmd("colorscheme sonokai")
-	end,
+	"shaunsingh/nord.nvim",
+	{ url = "https://codeberg.org/jthvai/lavender.nvim" },
+	{
+		"sainnhe/sonokai",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme sonokai")
+		end,
+	},
 }
