5db27645a82a80a5ba116183bd7b89d96a0255fe
Jason Tomsic <jasontomsic@quickenloans.com>
2024-03-31 13:59:36 -0400

    session handler

--- lua/plugins/greeter.lua
+++ lua/plugins/greeter.lua
@@ -14,4 +14,55 @@
 --]]
 
 return {
+	{
+		"echasnovski/mini.sessions",
+		version = false,
+		config = function()
+			local sessions = require("mini.sessions")
+			local directory = vim.fn.stdpath('data') .. '/session'
+
+			sessions.setup({
+				directory = directory,
+				file = '',
+			})
+
+			vim.keymap.set("n", "<leader>sl", function() sessions.select() end)
+			vim.api.nvim_create_user_command("SessionFolder", "e " .. directory, {})
+
+			vim.api.nvim_create_user_command("SessionCreate", function(args)
+				sessions.write(args.fargs[1], { force = false })
+			end, { nargs = 1 })
+
+			vim.api.nvim_create_user_command("SessionDelete", function(args)
+				sessions.select("delete", { force = args.bang })
+			end, { bang = true })
+		end,
+	},
+
+	-- {
+	-- 	"nvimdev/dashboard-nvim",
+	-- 	event = "VimEnter",
+	-- 	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	--
+	-- 	config = function()
+	-- 		require("dashboard").setup({})
+	-- 	end,
+	-- },
+	-- {
+	-- 	"goolord/alpha-nvim",
+	-- 	dependencies = {
+	-- 		"nvim-tree/nvim-web-devicons",
+	-- 		"nvim-lua/plenary.nvim",
+	-- 	},
+	-- 	config = function()
+	-- 		require("alpha").setup(require("alpha.themes.theta").config)
+	-- 	end,
+	-- },
+	-- {
+	-- 	'echasnovski/mini.starter',
+	-- 	version = false,
+	-- 	config = function()
+	-- 		require("mini.starter").setup()
+	-- 	end
+	-- },
 }
