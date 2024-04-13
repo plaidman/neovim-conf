13f4e55bf4c714aebb4c54fb396330011065bd1d
Jason Tomsic <jasontomsic@quickenloans.com>
2024-04-13 19:16:24 -0400

    switch to mini.files

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -1,29 +1,79 @@
 return {
 	{
-		"stevearc/oil.nvim",
+		"echasnovski/mini.files",
+		version = false,
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 
 		config = function()
-			require("oil").setup({
-				keymaps = {
-					["g?"] = "actions.show_help",
-					["<CR>"] = "actions.select",
-					["<leader>v"] = "actions.select_vsplit",
-					["<leader>s"] = "actions.select_split",
-					["<leader>p"] = "actions.preview",
-					["<leader>q"] = "actions.close",
-					["<leader>r"] = "actions.refresh",
-					["-"] = "actions.parent",
-					["_"] = "actions.open_cwd",
-					["`"] = "actions.cd",
-					["<leader>."] = "actions.toggle_hidden",
+			require("mini.files").setup({
+				mappings = {
+					close = "q",
+					go_in = "<s-enter>",
+					go_in_plus = "<enter>",
+					go_out = "",
+					go_out_plus = "<s-left>",
+					reset = "<bs>",
+					reveal_cwd = "_",
+					show_help = "g?",
+					synchronize = "=",
+					trim_left = "<",
+					trim_right = ">",
 				},
-				use_default_keymaps = false,
+				windows = {
+					preview = true,
+					width_preview = 80,
+				},
 			})
 
-			vim.api.nvim_create_user_command("OilCwd", "Oil .", {})
-			vim.api.nvim_create_user_command("OilFileDir", "Oil", {})
+			local show_dotfiles = true
+
+			local filter_show = function()
+				return true
+			end
+
+			local filter_hide = function(fs_entry)
+				return not vim.startswith(fs_entry.name, ".")
+			end
+
+			local toggle_dotfiles = function()
+				show_dotfiles = not show_dotfiles
+				local new_filter = show_dotfiles and filter_show or filter_hide
+				MiniFiles.refresh({ content = { filter = new_filter } })
+			end
+
+			local go_in_folder_only = function()
+				local type = MiniFiles.get_fs_entry().fs_type
+				if type == "file" then return end
+
+				MiniFiles.go_in({})
+			end
+
+			local open_mini_files_curfile = function()
+				MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+			end
+
+			local open_mini_files_cwd = function()
+				MiniFiles.open(nil, false)
+			end
+
+			local change_dir = function()
+				local cur_entry_path = MiniFiles.get_fs_entry().path
+				local cur_directory = vim.fs.dirname(cur_entry_path)
+				print("changed to" .. cur_directory)
+				vim.fn.chdir(cur_directory)
+			end
+
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "MiniFilesBufferCreate",
+				callback = function(args)
+					vim.keymap.set("n", "<s-right>", go_in_folder_only, { buffer = args.data.buf_id })
+					vim.keymap.set("n", "`", change_dir, { buffer = args.data.buf_id })
+					vim.keymap.set('n', 'g.', toggle_dotfiles, { buffer = args.data.buf_id })
+				end,
+			})
+
+			vim.keymap.set("n", "<leader>fe", open_mini_files_cwd, { desc = "[F]ile [E]xplorer (cwd)" })
+			vim.keymap.set("n", "-", open_mini_files_curfile, { desc = "[F]ile [E]xplorer (filepath)" })
 		end,
 	},
-
 }
